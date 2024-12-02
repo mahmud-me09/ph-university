1d375a4b6fa59f3a4203b38209235a2f82b6e916
mahmud-me09 <mahmud.me09@gmail.com>
2024-12-03 00:40:10 +0600

    validation working

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,45 @@
+import { catchAsync } from '../../utils/catchAsync';
+import { successResponse } from '../../utils/sendResponse';
+import { StudentServices } from './student.service';
+import httpStatusCodes from 'http-status-codes';
+
+const getSingleStudent = catchAsync(async (req, res) => {
+  const { studentId } = req.params;
+  const result = await StudentServices.getSingleStudentFromDB(studentId);
+
+  successResponse(res, {
+    statusCode: httpStatusCodes.OK,
+    success: true,
+    message: 'Student is retrieved succesfully',
+    data: result,
+  });
+});
+
+const getAllStudents = catchAsync(async (req, res) => {
+  const result = await StudentServices.getAllStudentsFromDB();
+
+  successResponse(res, {
+    statusCode: httpStatusCodes.OK,
+    success: true,
+    message: 'Student are retrieved succesfully',
+    data: result,
+  });
+});
+
+const deleteStudent = catchAsync(async (req, res) => {
+  const { studentId } = req.params;
+  const result = await StudentServices.deleteStudentFromDB(studentId);
+
+  successResponse(res, {
+    statusCode: httpStatusCodes.OK,
+    success: true,
+    message: 'Student is deleted succesfully',
+    data: result,
+  });
+});
+
+export const StudentControllers = {
+  getAllStudents,
+  getSingleStudent,
+  deleteStudent,
+};
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,7 +1,20 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { UserController } from './user.controller';
+import { AnyZodObject } from 'zod';
 
 const router = express.Router();
+
+const validateRequest = (schema: AnyZodObject) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const zodParsedData = await schema.parseAsync({
+        body: req.body,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+};
 
 router.post('/create-student', UserController.createStudent);
 // router.post('/create-faculty', UserController.createFaculty);
